139ed67fe3ccb921df1a7338c0c35c3b13fe7e49
Kieran <kieran.caruana@ada.ac.uk>
2021-03-04 17:50:47 +0000

    Menu files setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "board.h"
+#include "menu.h"
 #include "libarys/ini.h"
+
 int main() {
   std::cout << "Starting up...\n";
+  menu();
   boardSetup();
 }
--- menu.cpp
+++ menu.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <vector>
 
-int menu(){
+std::vector<std::string> modes{"One Player VS Computer", "Two Player Game", "One Player VS Computer (salvo)", "Two player game (salvo)", "One player v computer (hidden mines)", "Two player game (hidden mines)", "Computer v computer (hidden mines)", "Quit"}; 
 
+int menu(){
+  std::cout << modes.size();
+  return modes.size();
 }
